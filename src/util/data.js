c35683e0f11bf7447f37f5d2545206649ac6d3c8
Kike <luis17erebsamen@gmail.com>
2024-06-12 14:37:08 -0600

    chore: Add Indicaciones component and update Card component styling

---
+++ src/util/data.js
@@ -0,0 +1,71 @@
+const data = {
+  nombreNovio: 'Kevin',
+  apellidoNovio: 'Adame',
+  nombreNovia: 'Karen',
+  lugar: 'San Pedro Garza García, NL',
+  apellidoNovia: 'García',
+  fecha: '1722123900',
+  itinerario: [
+    {
+      nombre: 'Boda civil',
+      lugar: 'Lobby Gran Salon',
+      hora: '17:45',
+      direccion: 'Club Industrial, Av. Parte Aguas 698',
+      imgLink: 'https://beourguest.com.mx/wp-content/uploads/2024/05/00543_civil.png',
+      mapsLink: 'https://goo.gl/maps/123456',
+    },
+    {
+      nombre: 'Recepción',
+      lugar: 'Gran Salon',
+      hora: '19:00',
+      direccion: 'Club Industrial, Av. Parte Aguas 698',
+      imgLink: 'https://beourguest.com.mx/wp-content/uploads/2024/05/00543_recep.png',
+      mapsLink: 'https://goo.gl/maps/456789',
+    },
+  ],
+  indicaciones: [
+    {
+      titulo: 'No niños',
+      descripcion: 'Gracias por tu comprensión',
+      imagen: 'https://beourguest.com.mx/wp-content/uploads/2024/05/00543_ninos.png'
+    },
+    {
+      titulo: 'Dress Code',
+      descripcion: 'Rigurosa Etiqueta',
+      imagen: 'https://beourguest.com.mx/wp-content/uploads/2024/05/00543_dc.png'
+    },
+  ],
+  confirmacion: [
+    {
+      nombre: 'Kevin Adame',
+      confirmado: true,
+      linkWhatsApp: 'https://wa.me/123456',
+    },
+    {
+      nombre: 'Karen García',
+      confirmado: true,
+      linkWhatsApp: 'https://wa.me/456789',
+    },
+  ],
+  datosBancarios:[
+    {
+      banco: 'Banamex',
+      sucursal: '1234',
+      cuenta: '5678',
+      clabe: '123456789012345678',
+      nombre: 'Kevin Adame',
+    },
+    {
+      banco: 'Bancomer',
+      sucursal: '4321',
+      cuenta: '8765',
+      clabe: '987654321098765432',
+      nombre: 'Karen García',
+    },
+  ],
+  invLink: 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20240727T234500Z%2F20240728T090000Z&details=Los%20mejores%20momentos%20de%20la%20vida%20merecen%20ser%20compartidos%2C%20gracias%20por%20celebrar%20con%20nosotros.&location=San%20Pedro%20Garza%20Garc%C3%ADa&text=Boda%20Karen%20%26%20Kevin'
+  
+};
+
+
+export default data;
